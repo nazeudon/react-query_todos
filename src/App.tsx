8e178f942416418f72f08fe90369118ff4ee90d2
nazeudon <nazeudon48112@icloud.com>
2021-06-05 22:35:03 +0900

    init settings for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { VFC } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-function App() {
-  return <div className=""></div>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App: VFC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-mono text-sm text-gray-600">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default App
